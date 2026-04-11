8f31ecf3a7c1ecef58dca411ecd004dd490a545b
Rabbanihosni10 <rabbanihosni10@gmail.com>
2026-04-11 07:24:22 +0600

    first commit

---
+++ module_3/data_type.js
@@ -0,0 +1,28 @@
+let data=4;
+data='Tormuj';
+data={name:'bangi',price: 30};
+data=false;
+data=[1,1,2,445];
+data=undefined;
+data= null;
+data= 4.45;
+data=NaN;
+console.log(typeof data);
+
+
+// premitive dataTypes
+// usually any unique value that a variable carries is knowmn as primitive data types
+
+const price=40;
+const name2='Bulbul';
+const isPoor=false;
+let isKnown= undefined; //usually dont should set value to undefined
+let isLocal;
+const merit =null;
+console.log(isKnown, isLocal);
+
+//non premitive datatypes
+// usually dont carrty single unique value
+ 
+const student={name:'badal', location: 'Bristi goli', subject: 'botany'};
+const Friends=['tom', 'bob','jack'];
