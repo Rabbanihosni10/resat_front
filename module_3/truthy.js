e159b6bc41ca41c68a88b351f743836346e03769
RabaniHosni10 <rabbanihosni10@gmail.com>
2026-04-13 07:54:22 +0600

    truthy and falsy

--- module_3/truthy.js
+++ module_3/truthy.js
@@ -4,6 +4,8 @@ data=""; // its falsy
 data=" "; // it should be falsy but it has a space in the string so now its not a empty string so its truthy now
 data='0';
 data=false;
+data=true;
+data=[];
 console.log("Value of data:", data);
 
 if(data){
@@ -12,3 +14,18 @@ if(data){
 else{
     console.log(data,"Value of data is falsy");
 }
+// if i need to capture falsy value to go inside if block
+// use  logical not
+
+if(!data){
+    console.log("Inside if with a falsy value!");   
+}
+
+// capture all positive value
+//double not !!
+
+//capture any value to boolean (true false)
+if(!data === true){
+    console.log("Only True when inside the double not")
+}
+
