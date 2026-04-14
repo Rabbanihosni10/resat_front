02786dd21296329c2fc74ffb05c79920b421ec04
RabaniHosni10 <rabbanihosni10@gmail.com>
2026-04-15 04:44:02 +0600

    increment

---
+++ module_3/increment.js
@@ -0,0 +1,10 @@
+let counter=0;
+console.log(counter=counter+1);
+console.log(counter+=1);
+console.log(counter++); // post increment
+console.log(counter);
+console.log(++counter); // pre increment
+console.log(counter);
+
+// --counter // pre decrement
+// counter -- // post decrement
