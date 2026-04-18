41a075c2d8b0118ccd93dde36598fd0c0ed92e43
RabaniHosni10 <rabbanihosni10@gmail.com>
2026-04-18 07:28:43 +0600

    pass creation logics

--- short/pass_gen_py/pass_gen_py.py
+++ short/pass_gen_py/pass_gen_py.py
@@ -18,8 +18,26 @@ def generate_pass(min_length,numbers=True,spec_char=True):
     has_spec_char=False
 
     while not meet_criteria or len(pass_wd)<min_length:
-        new_char=random.choice(charateres)
+        new_char=random.choice(characters);
+        pass_wd+=new_char
+
+        if new_char in digits:
+            has_number=True
+        elif new_char in special:
+            has_spec_char=True
         
+        meets_criteria=True
+        if numbers:
+            meets_criteria=has_number
+        if spec_char:
+            meets_criteria=meets_criteria and has_spec_char
+    return pass_wd    
+
+
+
+
+        # new_char=random.choice(charateres)
+
     # pass
 
 # generate_pass(10,False,False)
